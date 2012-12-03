0dc9e3f64bcad12d040a8be01c355aa4ff084ebb
Adam Hutchison <adam@moneydesktop.com>
2012-12-02 17:58:33 -0700

    Add an explicit require for bundler and geminabox to ensure they get loaded first. Also removed the install method since we can just require bundler_geminabox/gem_tasks now.

--- lib/bundler_geminabox.rb
+++ lib/bundler_geminabox.rb
@@ -1,11 +1,4 @@
+require "bundler"
+require "geminabox"
+
 require "bundler_geminabox/version"
-
-
-module BundlerGeminabox
-  # Your code goes here...
-  class GemHelper
-    def self.install
-      require "bundler_geminabox/ext/bundler/gem_helper"
-    end
-  end
-end
