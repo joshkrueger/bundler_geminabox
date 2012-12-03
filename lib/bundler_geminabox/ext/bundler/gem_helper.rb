8726fe8e24f75ae080658ae0b46e60764886b917
Adam Hutchison <adam@moneydesktop.com>
2012-12-02 17:55:31 -0700

    Override the install method so we can reset the description on the release task to reference geminabox instead of Rubygems.

--- lib/bundler_geminabox/ext/bundler/gem_helper.rb
+++ lib/bundler_geminabox/ext/bundler/gem_helper.rb
@@ -1,10 +1,30 @@
+require 'bundler/gem_helper'
+
 # Lets just hijack the rubygem_push method.
 # Should probably alias it so we can switch on a flag, but... whatever.
 # I'll do it later.
 
 module Bundler
   class GemHelper
-    protected
+    def install
+      desc "Build #{name}-#{version}.gem into the pkg directory."
+      task 'build' do
+        build_gem
+      end
+
+      desc "Build and install #{name}-#{version}.gem into system gems."
+      task 'install' do
+        install_gem
+      end
+
+      desc "Create tag #{version_tag} and build and push #{name}-#{version}.gem to your geminabox server"
+      task 'release' do
+        release_gem
+      end
+
+      GemHelper.instance = self
+    end
+
     def rubygem_push(path)
       if Pathname.new("~/.gem/geminabox").expand_path.exist?
         sh("gem inabox #{path}")
